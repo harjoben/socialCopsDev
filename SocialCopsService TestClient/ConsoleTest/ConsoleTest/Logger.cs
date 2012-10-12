678c8a426bd9f92b6e070de655ba03454229273d
Varun <bankavarun@gmail.com>
2012-10-13 03:30:32 +0800

    Error Handling - CoreService

---
+++ SocialCopsService TestClient/ConsoleTest/ConsoleTest/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Logger
+    {
+
+        public void error(string message)
+        {
+            //
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message); // <-- see note
+            //
+            // Reset the color.
+            //
+            Console.ResetColor();
+        }
+
+        public void warning(string message)
+        {
+            //
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message); // <-- see note
+            //
+            // Reset the color.
+            //
+            Console.ResetColor();
+        }
+
+
+        public void start(string message)
+        {
+            //
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message.PadRight(Console.WindowWidth - 1)); // <-- see note
+            //
+            // Reset the color.
+            //
+            Console.ResetColor();
+        }
+    }
+}
---
+++ SocialCopsService TestClient/ConsoleTest/ConsoleTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleTest.CoreService;
+using System.ServiceModel;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        CoreClient client = null;
+        Logger logger = new Logger();
+
+        public void TestConnection()
+        {
+
+            logger.warning("Enter TestConnection Method");
+            try
+            {
+                client = new CoreClient();
+                TestConnectionRequest request = new TestConnectionRequest();
+                TestConnectionResponse response;
+                response = client.TestConnection(request);
+                Console.WriteLine("Testing Connection Result : " + response.TestConnectionResult);
+                logger.warning("Exit TestConnection Method");
+                Console.ReadLine();
+                
+            }
+            catch (FaultException<Bug> Fex)
+            {
+                logger.error("ErrorMessage::" + Fex.Detail.ErrorMessage + Environment.NewLine);
+                logger.error("ErrorDetails::" + Environment.NewLine + Fex.Detail.ErrorDetails);
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (null != client)
+                {
+                    client.Close();
+                }
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            test.logger.start("Social Cops Service Test Client");
+            test.TestConnection();
+        }
+    }
+}
+
