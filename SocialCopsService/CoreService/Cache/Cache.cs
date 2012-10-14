9b3d5f05078e61fcfbf063164801c94b732c4281
Varun <bankavarun@gmail.com>
2012-10-15 05:45:47 +0800

    Cache Class

---
+++ SocialCopsService/CoreService/Cache/Cache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoreService.Cache
+{
+    public class Cache
+    {
+        public void AddToCache(object key, object temp)
+        {
+            if (CachingConfig.CachingEnabled)
+            {
+                if (CachingConfig.SlidingExpirationTime <= 0 || CachingConfig.SlidingExpirationTime == int.MaxValue)
+                {
+                    WCFCache.Current[key] = temp;
+                }
+                else
+                {
+                    WCFCache.Current.Insert(key, temp, new TimeSpan(0, 0, CachingConfig.SlidingExpirationTime), true);
+                }
+            } 
+        }
+    }
+}
